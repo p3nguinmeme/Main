35a883d1954a1220898341cd04312fc16d3bfbf5
p3nguinmeme <tayganleach@gmail.com>
2023-11-18 22:53:08 +0000

    Update to progress
    Considering murdering a child rn
    
    IF ANYONE KNOWS HOW TO GET JS TO DO NON-BUSY SLEEP, PLEASE FUCKING TELL ME

--- TL/animation.js
+++ TL/animation.js
@@ -6,52 +6,64 @@ canvas.height = window.innerHeight;
 
 //This is the object that is a WIP
 let obj = {
-    object: document.getElementById("animate1"),
+    target: document.getElementById("animate1"),
     posx: 0,
     posy: 0,
     velocity: 10,
     speed: 1,
     sprite: "",
-    up: function(length) {
-        stepCount = Math.ceil(length / (this.velocity * this.speed))
-        stepLength = length / stepCount
-        for (let i = 0; i < stepCount; i++) {
-          this.posy = this.posy - stepLength
-
-          this.object.style.top = this.posy + "px"
-          this.object.style.left = this.posx + "px"
-
-
-          console.log(this.posy)
-          //console.log(stepCount)
-          //console.log(stepLength)
-        }
-    },
-    down: function(length) {
+    up: async function(length) {
+      console.log("up")
       stepCount = Math.ceil(length / (this.velocity * this.speed))
       stepLength = length / stepCount
-
-      id1 = setInterval(this.frame, 100);
+      direction = "up"
+      this.frame(this, stepCount, stepLength, direction)
+      sleep(100 * stepCount)
+      //id1 = setInterval(this.frame.bind(this, stepCount, stepLength, direction), 100);
       
-      
-    
     },
-    frame: function() {
+    down: async function(length) {
+      console.log("down")
+      stepCount = Math.ceil(length / (this.velocity * this.speed))
+      stepLength = length / stepCount
+      direction = "down"
+      this.frame(this, stepCount, stepLength, direction)
+      sleep(100 * stepCount)
+      //id1 = setInterval(this.frame.bind(this, stepCount, stepLength, direction), 100);
+      
+    },
+    frame: async function() {
+      while (true){
       if (stepCount > 0) {
-        console.log(stepCount)
+        await sleep(100)
+        console.log("moving", direction)
+
+        switch(direction) {
+          case "up":
+            this.posy = this.posy - stepLength
+            break;
+          case "down":
+            this.posy = this.posy + stepLength
+            break;
+          case "left":
+            this.posx = this.posx - stepLength
+            break;
+          case "right":
+            this.posx = this.posx + stepLength
+            break;
+          default:
+            console.log("This shouldnt fire");
+        }
         stepCount = stepCount - 1
-        
-        this.posy = this.posy + stepLength
 
         //These two lines suffer the issue that im struggling to fix
-        this.object.style.top = this.posy + "px"
-        this.object.style.left = this.posx + "px"
+        this.target.style.top = this.posy + "px"
+        this.target.style.left = this.posx + "px"
 
       } else{
-        clearInterval(id1)
-        console.log("Ended")
-        
-      }
+        console.log("Ended", direction )
+        return "done"
+      }}
       
   }
 };
@@ -59,10 +71,23 @@ let obj = {
 let obj1 = Object.create(obj);
 //overwriting position
 obj1.posx = 2;
-//call function with argument
-obj1.down(100);
 
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//Im loosing the plot as we speak
+async function run() {
+  await obj.up(200)
+
+  await obj.down(100)
+}
+
+
+
+run()
+console.log("This should be seen after everything!")
 //You can ignore essentially everything below here, Its only being used as a reference at the moment so try to keep it in
 var id = null;
 function myMove() {
