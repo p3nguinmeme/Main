39f6f75eb76f34642617fed7c2ea3fec41278a43
p3nguinmeme <tayganleach@gmail.com>
2023-11-19 19:53:55 +0000

    Animation system Fully* functional!
    works on any target,
    Note: sprite work needs to be implemented

--- TL/animation.js
+++ TL/animation.js
@@ -6,127 +6,117 @@ canvas.height = window.innerHeight;
 
 //This is the object that is a WIP
 let obj = {
+    id: 0,
     target: document.getElementById("animate1"),
     posx: 0,
     posy: 0,
     velocity: 10,
     speed: 1,
     sprite: "",
+    framerate: 45,
+    direction: "",
+    stepCount: 0,
+    stepLength: 0,
     up: async function(length) {
-      console.log("up")
-      stepCount = Math.ceil(length / (this.velocity * this.speed))
-      stepLength = length / stepCount
-      direction = "up"
-      this.frame(this, stepCount, stepLength, direction)
-      sleep(100 * stepCount)
+      this.stepCount = Math.ceil(length / (this.velocity * this.speed))
+      this.stepLength = length / this.stepCount
+      this.direction = "up"
+      this.frame(this)
+      await sleep((this.framerate + 5) * this.stepCount)
       //id1 = setInterval(this.frame.bind(this, stepCount, stepLength, direction), 100);
       
     },
     down: async function(length) {
-      console.log("down")
-      stepCount = Math.ceil(length / (this.velocity * this.speed))
-      stepLength = length / stepCount
-      direction = "down"
-      this.frame(this, stepCount, stepLength, direction)
-      sleep(100 * stepCount)
+      this.stepCount = Math.ceil(length / (this.velocity * this.speed))
+      this.stepLength = length / this.stepCount
+      this.direction = "down"
+      this.frame(this)
+      await sleep((this.framerate + 5) * this.stepCount)
+      //id1 = setInterval(this.frame.bind(this, stepCount, stepLength, direction), 100);
+      
+    },
+    left: async function(length) {
+      this.stepCount = Math.ceil(length / (this.velocity * this.speed))
+      this.stepLength = length / this.stepCount
+      this.direction = "left"
+      this.frame(this)//, stepCount, stepLength, direction)
+      await sleep((this.framerate + 5) * this.stepCount)
+      //id1 = setInterval(this.frame.bind(this, stepCount, stepLength, direction), 100);
+      
+    },
+    right: async function(length) {
+      this.stepCount = Math.ceil(length / (this.velocity * this.speed))
+      this.stepLength = length / this.stepCount
+      this.direction = "right"
+      this.frame(this)
+      await sleep((this.framerate + 5) * this.stepCount)
       //id1 = setInterval(this.frame.bind(this, stepCount, stepLength, direction), 100);
       
     },
     frame: async function() {
       while (true){
-      if (stepCount > 0) {
-        await sleep(100)
-        console.log("moving", direction)
+      if (this.stepCount > 0) {
+        await sleep(this.framerate)
+        console.log(this.id, "moving", this.direction)
 
-        switch(direction) {
+        switch(this.direction) {
           case "up":
-            this.posy = this.posy - stepLength
+            this.posy = this.posy - this.stepLength
             break;
           case "down":
-            this.posy = this.posy + stepLength
+            this.posy = this.posy + this.stepLength
             break;
           case "left":
-            this.posx = this.posx - stepLength
+            this.posx = this.posx - this.stepLength
             break;
           case "right":
-            this.posx = this.posx + stepLength
+            this.posx = this.posx + this.stepLength
             break;
           default:
             console.log("This shouldnt fire");
         }
-        stepCount = stepCount - 1
+        this.stepCount = this.stepCount - 1
 
         //These two lines suffer the issue that im struggling to fix
         this.target.style.top = this.posy + "px"
         this.target.style.left = this.posx + "px"
 
       } else{
-        console.log("Ended", direction )
+        console.log("Ended", this.direction )
         return "done"
       }}
       
   }
 };
-//inherits base values given
+
+//This section is used to initialise Any animated object and any specific propertiess
+
+//obj1  =Main character to be moved
 let obj1 = Object.create(obj);
-//overwriting position
-obj1.posx = 2;
+obj1.id = 1;
 
+//obj2 =links 
+let obj2 = Object.create(obj);
+obj2.target = document.getElementById("links")
+obj2.id = 2;
+obj2.posy = canvas.height
 
+//We're So back!
+//await is used to wait for a thing to finish
+async function run() {
+  //these two will fire in parralel
+  obj1.down(200)
+  await obj2.up(200)
+
+  await obj1.right(200)
+  await obj1.left(100)
+  
+}
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-//Im loosing the plot as we speak
-async function run() {
-  await obj.up(200)
-
-  await obj.down(100)
-}
-
-
 
 run()
-console.log("This should be seen after everything!")
+
 //You can ignore essentially everything below here, Its only being used as a reference at the moment so try to keep it in
-var id = null;
-function myMove() {
-  var obj = document.getElementById("animate1"); 
-  var frame = 0; 
-  var posx = 0;
-  var posy = 0;
-  id = setInterval(getframe, 10);
-
-
-
-  
-  function getframe() {
-      if (frame < 100){
-        frame++;
-
-        posx++;
-        obj.style.top = posx + "px";
-        obj.style.left = posy + "px";
-
-      } else{
-        clearInterval(id);
-      }
-      drawCharacters();
-  }
-  function drawCharacters(){
-    obj.style.top = posx + "px";
-    obj.style.left = posy + "px";
-  }
-
-  /* This section is to create a movement system to help animate */
-  function moveup(object, amount){
-    object.style.top
-  }
-}
-
-//myMove();
-
-function draw(object, posx, posy){
-  object.object.style.top = posy + "px"
-  object.object.style.left = posx + "px"
-}
